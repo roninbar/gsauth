899a99907b5d3a07fc3ab276ac2cb67aa7caa1cd
ron <rinbar70@gmail.com>
2022-11-27 22:17:09 +0200

    Generate a signed URL for Cloud Storage.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,19 @@ const { Storage } = require('@google-cloud/storage');
 const express = require('express');
 const router = express.Router();
 
+const GS_BUCKET = process.env['GS_BUCKET'] || 'bucket';
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
     const gs = new Storage();
     const credentials = await gs.authClient.getCredentials();
-    res.render('index', { title: 'Express', ...credentials });
+    const [url] = await gs.bucket(GS_BUCKET).file('0.txt').getSignedUrl({
+        version: 'v4',
+        action: 'write',
+        expires: Date.now() + 60_000,
+        contentType: 'text/plain',
+    });
+    res.render('index', { title: 'Express', ...credentials, url });
 });
 
 module.exports = router;
