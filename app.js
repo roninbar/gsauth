6f763b37b8a14adbf83132a188f6806202ccc7f7
ron <rinbar70@gmail.com>
2022-11-28 15:32:25 +0200

    Fix a bug in the initialization of the logger.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger(logger(ENVIRONMENT.startsWith('dev') ? 'dev' : 'common')));
+app.use(logger(ENVIRONMENT.startsWith('dev') ? 'dev' : 'common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
